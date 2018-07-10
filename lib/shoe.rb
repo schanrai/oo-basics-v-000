631ec702040eeaab96d09733cc85b84022143c4e
Sushi Chanrai <sushi@pathworks.co>
2018-07-10 02:15:44 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+
+#  def brand
+#    @brand
+#  end
+
+end
